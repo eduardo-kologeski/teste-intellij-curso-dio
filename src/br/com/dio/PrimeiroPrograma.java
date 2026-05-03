d0c431d5392510a4783ffffeb9aefa8edb128a3c
eduardo-kologeski <eduardokologeski@outlook.com>
2026-05-03 03:37:30 -0300

    feat: adicionando a classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,10 +1,13 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+        Livro livro = new Livro("Metamorfose", 300);
+        System.out.println(livro.toString());
     }
 }
